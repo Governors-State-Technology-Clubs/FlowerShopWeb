635f4321ba66414463529c80eec06666e0c05c63
JoBewley <166559708+JoBewley@users.noreply.github.com>
2026-03-24 20:32:54 -0500

    updated files to work when running app.py and corrected image and css links.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask ,url_for, render_template
+from flask import Flask ,url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/services', methods=["GET", "POST"])
+def services():
+    return render_template('services.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
