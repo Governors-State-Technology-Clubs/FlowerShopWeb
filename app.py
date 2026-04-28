96db4fd340a1ede8587aad3ead69df8cce5d04d6
Mario Mendez <mendezmario2018@gmail.com>
2026-04-28 18:19:14 -0500

    added logo and about us and availability

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask ,url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about_us.html')
+
+@app.route('/availability')
+def availability():
+    return render_template('availability.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
